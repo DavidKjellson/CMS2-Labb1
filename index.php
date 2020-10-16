63ef029a7172e7f67a2d86fa4fd575ef09bdfb3e
David Kjellson <david.kjellson@gmail.com>
2020-10-16 16:03:49 +0200

    Better Sandwich class

--- index.php
+++ index.php
@@ -1,41 +1,16 @@
-<h1>Uppgift 1</h1>
-<h3><?php echo uppgift1("Hello PHP-hell!"); ?></h3>
-<h1>Uppgift 2</h1>
-<?php echo uppgift2("Hello kitchen!", 1); ?>
-<h1>Uppgift 3</h1>
-<h3><?php echo eat(); ?></h3>
-
-<?php function uppgift1($message)
-{
-  return "$message";
-}
-function uppgift2($message, $headerprop)
-{
-  return '<h' . $headerprop . '>' . $message . '</h' . $headerprop . '>';
-}
+<?php
 class Food
 {
   public $name;
-  public function __construct($name)
+  public function eat()
   {
-    $this->name = $name;
-  }
-  public function food()
-  {
-    echo $this->name;
+    echo "Nu åts en " . $this->name . ".";
   }
 }
 class Sandwich extends Food
 {
-  public function eatsand()
-  {
-    echo "Nu åts en ";
-  }
+  public $mackname;
 }
-function eat()
-{
-  $macka = new Sandwich("macka");
-  echo $macka->eatsand();
-  echo $macka->food();
-}
-?>
+$macka = new Sandwich();
+$macka->name = 'macka';
+$macka->eat();
