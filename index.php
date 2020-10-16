653a3676cf6c052580c0eab5f9a67d4a3c563698
David Kjellson <david.kjellson@gmail.com>
2020-10-16 11:10:09 +0200

    Class Food

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <h1>Uppgift 1</h1>
-<h3><?php echo uppgift1("Hello world!"); ?></h3>
+<h3><?php echo uppgift1("Hello PHP-hell!"); ?></h3>
 <h1>Uppgift 2</h1>
 <?php echo uppgift2("Hello kitchen!", 1); ?>
+<h1>Uppgift 3</h1>
+<h3><?php echo eat(); ?></h3>
 
 <?php function uppgift1($message)
 {
@@ -10,5 +12,23 @@
 function uppgift2($message, $headerprop)
 {
   return '<h' . $headerprop . '>' . $message . '</h' . $headerprop . '>';
+}
+class Food
+{
+  public $name;
+  function set_name($name)
+  {
+    $this->name = $name;
+  }
+  function get_name()
+  {
+    return $this->name;
+  }
+}
+function eat()
+{
+  $macka = new Food();
+  $macka->set_name("macka");
+  return "Nu åts en " . $macka->get_name();
 }
 ?>
