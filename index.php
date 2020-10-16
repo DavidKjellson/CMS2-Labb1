d5905cd19bf4ed3ed7ad7c8b1d93de10bacf273b
David Kjellson <david.kjellson@gmail.com>
2020-10-16 10:40:22 +0200

    Return string and header parameter function

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <h1>Uppgift 1</h1>
-<h3><?php echo uppgift1(); ?></h3>
+<h3><?php echo uppgift1("Hello world!"); ?></h3>
+<h1>Uppgift 2</h1>
+<?php echo uppgift2("Hello kitchen!", 1); ?>
 
-<?php function uppgift1()
+<?php function uppgift1($message)
 {
-  return "Hello world!";
-} ?>
+  return "$message";
+}
+function uppgift2($message, $headerprop)
+{
+  return '<h' . $headerprop . '>' . $message . '</h' . $headerprop . '>';
+}
+?>
